5c038cdf887426e3f89a28c47a8ea919e7c9175d
Ronald Wong <ronaldchunyin@gmail.com>
2018-07-11 17:30:46 +0800

    Removed PostgreSQL settings

--- config/database.php
+++ config/database.php
@@ -1,9 +1,4 @@
 <?php
-$url = parse_url(getenv("HEROKU_POSTGRESQL_ROSE_URL"));
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
-$database = substr($url["path"], 1) ?? null;
 
 return [
     /*
@@ -71,17 +66,6 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
-
-        'pgsql_production' => [
-            'driver' => 'pgsql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
         ],
 
         'sqlsrv' => [
